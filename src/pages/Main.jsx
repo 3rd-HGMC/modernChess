a30572541a35855cbff26c2d472b7beca41aff36
woohyunjng <woohyun.jng08@gmail.com>
2024-01-21 02:12:05 +0900

    feat(pages/Main): create main page menu

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "../styles/Main.css";
+import Icon from "../assets/icon.png";
+import PlayIcon from "../assets/play.png";
+import HelpIcon from "../assets/help.png";
 
 const Main = () => (
-    <div className="text-center">
-        <h1>Hello, Moomin!</h1>
+    <div className="container">
+        <div className="menu">
+            <img className="icon" src={Icon} />
+            <div className="menu">
+                <Link to="/play">
+                    <button type="button" className="btn playBtn">
+                        <img src={PlayIcon} />
+                    </button>
+                </Link>
+                <Link to="/help">
+                    <button type="button" className="btn helpBtn">
+                        <img src={HelpIcon} />
+                    </button>
+                </Link>
+            </div>
+        </div>
     </div>
 );
 
