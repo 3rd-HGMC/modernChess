b7c158bb13adabf42b42de0e528e42830ea64593
woohyunjng <woohyun.jng08@gmail.com>
2024-01-28 01:35:18 +0900

    feat(pages/GamePlay): event panel design

--- src/pages/GamePlay.jsx
+++ src/pages/GamePlay.jsx
@@ -10,8 +10,8 @@ import Blue from "../assets/blue.png";
 import UnitsIcon from "../assets/units.png";
 import EventIcon from "../assets/event.png";
 import BuildingsIcon from "../assets/buildings.png";
-import OppBuildingsIcon from "../assets/oppbuildings.png";
-import BackIcon from "../assets/back.png";
+import NextIcon from "../assets/next.png";
+import EventOption from "../assets/eventoption.png";
 
 const GamePlay = () => {
     const [board, setBoard] = useState(Array.from({ length: 80 }, () => 0));
@@ -19,6 +19,9 @@ const GamePlay = () => {
     const [turn, setTurn] = useState(1);
     const [hp, setHP] = useState(10);
     const [money, setMoney] = useState(10);
+
+    const [type, setType] = useState(0); // 0: event 1: units 2: buildings
+    const [events, setEvents] = useState([0, 1, 2]);
 
     return (
         <div className="grid">
@@ -49,26 +52,60 @@ const GamePlay = () => {
             </div>
             <div className="panel" style={{ backgroundImage: `url(${Panel})` }}>
                 <div className="info">
-                    <div className="infoDetail">턴 : {turn}</div>
-                    <div className="infoDetail">체력 : {hp}</div>
-                    <div className="infoDetail">돈 : {money}</div>
+                    <div className="infoDetail detail">턴 : {turn}</div>
+                    <div className="infoDetail detail">체력 : {hp}</div>
+                    <div className="infoDetail detail">돈 : {money}</div>
                 </div>
-                <div className="gamePlay"></div>
+                <div className="gamePlay">
+                    {type === 0 ? (
+                        <div className="events">
+                            <div className="eventDetail detail">
+                                {"<"}이벤트 발생문구{">"}
+                            </div>
+                            {events.map((event) => (
+                                <button
+                                    key={`eventbtn${event}`}
+                                    type="button"
+                                    className="eventButton btn"
+                                    onClick={() => 0}
+                                    style={{
+                                        backgroundImage: `url(${EventOption})`,
+                                    }}
+                                >
+                                    Event #01
+                                </button>
+                            ))}
+                        </div>
+                    ) : type === 1 ? (
+                        <h1>1</h1>
+                    ) : (
+                        <h1>2</h1>
+                    )}
+                </div>
                 <div className="select">
-                    <button type="button" className="btn oppbuildingsBtn">
-                        <img src={OppBuildingsIcon} />
-                    </button>
-                    <button type="button" className="btn eventBtn">
+                    <button
+                        type="button"
+                        className="btn eventBtn"
+                        onClick={() => setType(0)}
+                    >
                         <img src={EventIcon} />
                     </button>
-                    <button type="button" className="btn unitsBtn">
+                    <button
+                        type="button"
+                        className="btn unitsBtn"
+                        onClick={() => setType(1)}
+                    >
                         <img src={UnitsIcon} />
                     </button>
-                    <button type="button" className="btn buildingsBtn">
+                    <button
+                        type="button"
+                        className="btn buildingsBtn"
+                        onClick={() => setType(2)}
+                    >
                         <img src={BuildingsIcon} />
                     </button>
-                    <button type="button" className="btn backBtn">
-                        <img src={BackIcon} />
+                    <button type="button" className="btn nextBtn">
+                        <img src={NextIcon} />
                     </button>
                 </div>
             </div>
