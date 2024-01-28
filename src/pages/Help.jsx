12b29a222336c23c40b699f1a96d13f63053927b
mini2317 <ekdmsdlfma@gmail.com>
2024-01-28 12:27:03 +0900

    test

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Help = () => (
-    <div className="text-center">
-        <h1>Hello, Moomin!</h1>
-    </div>
-);
+import * as tutorialImages from '../assets/tutorial/images';
+import * as tutorialButtons from '../assets/tutorial/buttons';
+
+const Help = () => {
+
+    const [image, setImage] = useState(0);
+
+    goBackward = () => image > 0 ? setImage({image} - 1) : 0;
+
+    return (
+        <div className="text-center">
+            <div className="top">
+                <Link to="../">
+                    <button type="button" className="btn back">
+                        <img src = {tutorialButtons["x.png"]} />
+                    </button>
+                </Link>
+                <h1>Tutorial</h1>
+            </div>
+            <div className="image">
+                <img src = {tutorialImages[`${image}.png`]} />
+            </div>
+            <div className="bottom">
+                <button
+                    type="button"
+                    className="backward"
+                    onClick = {
+                        () => ({image} > 0) ? setImage({image} - 1) : 0
+                    }
+                >
+                    <img src = {tutorialButtons[`00.png`]} />
+                </button>
+                <button
+                    type="button"
+                    className="forward"
+                    onClick = {
+                        () => ({image} < Object.keys(tutorialButtons).length - 1) ? setImage({image} + 1) : 0
+                    }
+                >
+                    <img src = {tutorialButtons[`10.png`]} />
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default Help;
