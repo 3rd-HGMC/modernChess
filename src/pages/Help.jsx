c473375ce6b1bba0a4ab654e921b79b9b259bb83
woohyunjng <woohyun.jng08@gmail.com>
2024-01-28 14:06:50 +0900

    feat(pages/Help): create Tutorial

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,46 +1,66 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import * as tutorialImages from '../assets/tutorial/images';
-import * as tutorialButtons from '../assets/tutorial/buttons';
+import "../styles/Help.css";
+import XBtn from "../assets/tutorial/x.png";
+import BackwardBtn from "../assets/tutorial/backward.png";
+import BackwardDisabledBtn from "../assets/tutorial/backward_disabled.png";
+import ForwardBtn from "../assets/tutorial/forward.png";
+import ForwardDisabledBtn from "../assets/tutorial/forward_disabled.png";
 
 const Help = () => {
-
     const [image, setImage] = useState(0);
-
-    goBackward = () => image > 0 ? setImage({image} - 1) : 0;
+    const maxTutorial = 25;
 
     return (
-        <div className="text-center">
-            <div className="top">
-                <Link to="../">
-                    <button type="button" className="btn back">
-                        <img src = {tutorialButtons["x.png"]} />
+        <div className="helpContainer">
+            <div className="interaction">
+                <div className="image">
+                    <img src={`/modernChess/images/${image}.png`} />
+                </div>
+                <div className="bottom">
+                    {
+                        <button
+                            type="button"
+                            className={`backward btn ${
+                                image > 0 ? "" : "disabled"
+                            }`}
+                            onClick={() =>
+                                image > 0 ? setImage(image - 1) : 0
+                            }
+                        >
+                            <img
+                                src={
+                                    image > 0
+                                        ? BackwardBtn
+                                        : BackwardDisabledBtn
+                                }
+                            />
+                        </button>
+                    }
+                    <button
+                        type="button"
+                        className={`forward btn ${
+                            image < maxTutorial ? "" : "disabled"
+                        }`}
+                        onClick={() =>
+                            image < maxTutorial ? setImage(image + 1) : 0
+                        }
+                    >
+                        <img
+                            src={
+                                image < maxTutorial
+                                    ? ForwardBtn
+                                    : ForwardDisabledBtn
+                            }
+                        />
                     </button>
-                </Link>
-                <h1>Tutorial</h1>
-            </div>
-            <div className="image">
-                <img src = {tutorialImages[`${image}.png`]} />
-            </div>
-            <div className="bottom">
-                <button
-                    type="button"
-                    className="backward"
-                    onClick = {
-                        () => ({image} > 0) ? setImage({image} - 1) : 0
-                    }
-                >
-                    <img src = {tutorialButtons[`00.png`]} />
-                </button>
-                <button
-                    type="button"
-                    className="forward"
-                    onClick = {
-                        () => ({image} < Object.keys(tutorialButtons).length - 1) ? setImage({image} + 1) : 0
-                    }
-                >
-                    <img src = {tutorialButtons[`10.png`]} />
-                </button>
+                    <Link to="../">
+                        <button type="button" className="btn">
+                            <img src={XBtn} />
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
